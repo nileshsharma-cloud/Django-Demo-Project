e60bd9c7db44f5af2cdec04014b18baf711f362e
Nilesh Sharma <53362304+nileshsharma-cloud@users.noreply.github.com>
2020-05-26 01:22:14 +0530

    Added another app module for signin and registration form.

---
+++ telusko/accounts/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+
+
+# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        email = request.POST['email']
+
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, "Username taken")
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.warning(request, 'email is already taken.')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
+                                                email=email, password=password1)
+                user.save()
+                print('user redirected')
+        else:
+            messages.info(request, 'Password not match.')
+            return redirect('register')
+        return redirect('/')
+
+    else:
+        return render(request, 'register.html')
--- telusko/telusko/urls.py
+++ telusko/telusko/urls.py
@@ -19,8 +19,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('travello', include('travello.urls')),
+    path('', include('travello.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls'))
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
