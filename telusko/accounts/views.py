eed908b969152351e581b0b854711506b05e4996
Nilesh Sharma <53362304+nileshsharma-cloud@users.noreply.github.com>
2020-05-27 01:12:00 +0530

    Login functionality added to the code.

--- telusko/accounts/views.py
+++ telusko/accounts/views.py
@@ -4,6 +4,22 @@ from django.contrib import messages
 
 
 # Create your views here.
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username = username,password = password)
+
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+
+        else:
+            messages.info(request,'Inavlid login')
+            return redirect('login')
+    else:
+        return render(request,'login.html')
 
 def register(request):
     if request.method == 'POST':
@@ -26,6 +42,7 @@ def register(request):
                                                 email=email, password=password1)
                 user.save()
                 print('user redirected')
+                return redirect('login')
         else:
             messages.info(request, 'Password not match.')
             return redirect('register')
